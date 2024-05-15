eb8fadd9e7d63639a02bac5a854499dd369cf855
ibraphem <ibraphem@gmail.com>
2024-05-15 23:41:41 +0100

    dialect issue fixed

--- utils/database.ts
+++ utils/database.ts
@@ -1,4 +1,4 @@
-import { Dialect, Sequelize } from 'sequelize'
+import { Sequelize } from 'sequelize'
 import dotenv from "dotenv"
 
 dotenv.config();
@@ -6,13 +6,12 @@ dotenv.config();
 const dbName = process.env.PG_DB as string
 const dbUser = process.env.PG_USER as string
 const dbHost = process.env.PG_HOST
-const dbDriver = process.env.PG_DRIVER as Dialect
 const dbPassword = process.env.PG_PASSWORD
 
 
 const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
-  dialect: dbDriver
+  dialect: "postgres"
 })
 
 export default sequelizeConnection
